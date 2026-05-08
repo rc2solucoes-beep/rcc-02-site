fc7cff9838a5e3e7f593971e9b0e509b6cc8f88b
Robson <robson.sazevedo@gmail.com>
2026-05-08 16:56:35 -0300

    fix: corrigir syntax error no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -182,7 +182,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             alt=""
           />
         </noscript>
-        <
+        <a
           href="#main-content"
           className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-[100] focus:px-4 focus:py-2 focus:bg-rc2-orange focus:text-white focus:text-sm focus:font-medium focus:rounded"
         >
